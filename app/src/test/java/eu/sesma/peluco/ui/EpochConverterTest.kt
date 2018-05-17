f6ff8e9b793cbcab27e157300ae3ffbef52831ee
Miguel Sesma <miguel.sesma@touchsurgery.com>
2018-05-17 16:47:39 +0100

    Add Epoch converter test

--- app/src/test/java/eu/sesma/peluco/ui/EpochConverterTest.kt
+++ app/src/test/java/eu/sesma/peluco/ui/EpochConverterTest.kt
@@ -1,9 +1,9 @@
 package eu.sesma.peluco.ui
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class EpochConverterTest {
-
 
     internal var converter = EpochConverter()
 
@@ -13,8 +13,6 @@ class EpochConverterTest {
 
         val result = converter.tm.toString()
 
-        println(result)
+        assertEquals("Tm{tm_sec=25, tm_min=46, tm_hour=17, tm_mday=30, tm_mon=4, tm_year=2018, tm_wday=1, tm_yday=120, tm_isdst=1}", result)
     }
-
-
 }
